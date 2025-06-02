7d9cf49ffb72ca203b99825c7021a7e9a1cfbe03
Gustavo Vieira <gustavieiradev@gmail.com>
2025-06-02 18:31:06 -0300

    feat: Criando arquivos em nossa instância com Terraform.

---
+++ Terraform e Ansible/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-075686beab831bb7f"
+  instance_type = "t2.micro"
+  key_name      = "iac-alura-gusta"
+
+  user_data = <<-EOF
+                 #!/bin/bash
+                 cd /home/ubuntu
+                 echo "<h1>Instância Alura Terraform - feito com Terraform</h1>" > index.html
+                 nohup busybox httpd -f -p 8080 &
+                 EOF
+
+
+  tags = {
+    Name = "Teste AWS Terraform"
+  }
+}
